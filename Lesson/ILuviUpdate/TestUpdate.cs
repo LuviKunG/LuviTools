1ccfa8549c6f2bd716a3533a2b343e6514eca39c
Thanut Panichyotai <luvikung@gmail.com>
2015-10-31 16:27:31 +0700

    - Add ILuviUpdate

---
+++ Lesson/ILuviUpdate/TestUpdate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUpdate : MonoBehaviour, ILuviUpdate
+{
+    public void Start()
+    {
+        Singleton<TestMainUpdate>.Instance.AddUpdate(this);
+    }
+
+    public void HookUpdate()
+    {
+
+    }
+}
---
+++ Lesson/ILuviUpdate/TestUpdateSecond.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUpdateSecond : MonoBehaviour, ILuviUpdate
+{
+    public void Start()
+    {
+        Singleton<TestMainUpdate>.Instance.AddUpdate(this);
+    }
+
+    public void HookUpdate()
+    {
+        
+    }
+}
