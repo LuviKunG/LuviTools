ba82a79435b25a2b43e0c1ce3c546a6772007936
Thanut Panichyotai <luvikung@gmail.com>
2019-12-26 14:30:58 +0700

    update datetime extension.

--- Extension/DateTimeExtension.cs
+++ Extension/DateTimeExtension.cs
@@ -6,7 +6,9 @@ namespace LuviKunG.Date
     public static class DateTimeExtension
     {
         private const string DATETIME_FORMAT_ISO8601 = "yyyy-MM-ddTHH\\:mm\\:ssK";
+        private const string DATETIME_FORMAT_ISO8601_MILLISECOND = "yyyy-MM-ddTHH\\:mm\\:ss.fffK";
 
-        public static string ISO8601(this DateTime dateTime) => dateTime.ToString(DATETIME_FORMAT_ISO8601, CultureInfo.InvariantCulture);
+        public static string ToISO8601(this DateTime dateTime) => dateTime.ToString(DATETIME_FORMAT_ISO8601, CultureInfo.InvariantCulture);
+        public static string ToISO8601Millisecond(this DateTime dateTime) => dateTime.ToString(DATETIME_FORMAT_ISO8601_MILLISECOND, CultureInfo.InvariantCulture);
     }
 }
